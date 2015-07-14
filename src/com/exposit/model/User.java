d18a0e2b689e039bb7f0e51eb1b3392d950c3559
Max <Max@192.168.1.4>
2015-07-14 18:22:16 +0200

    Adding User,Role classes

--- src/com/exposit/model/User.java
+++ src/com/exposit/model/User.java
@@ -8,6 +8,63 @@ public class User {
 	private String login;
 	private String password;
 	private String email;
-	
+
 	private Role role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 }
