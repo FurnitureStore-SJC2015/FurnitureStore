d7e65beb6212db6210cb72b3c9e83f22070436df
Max <Max@192.168.1.4>
2015-07-14 18:18:44 +0200

    Adding package for work! Let's do id!

---
+++ src/com/exposit/model/Role.java
@@ -0,0 +1,7 @@
+package com.exposit.model;
+
+public class Role {
+
+	private int id;
+	private String name;
+}
---
+++ src/com/exposit/model/User.java
@@ -0,0 +1,13 @@
+package com.exposit.model;
+
+public class User {
+
+	private int id;
+	private String name;
+	private String surname;
+	private String login;
+	private String password;
+	private String email;
+	
+	private Role role;
+}
