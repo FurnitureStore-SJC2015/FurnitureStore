557432a9d43d7517e35ca9c7f328945d0a282ba7
Max <Max@192.168.1.4>
2015-08-04 16:54:27 +0200

    Testing spring-mvc web example! Everything works fine!

--- src/main/java/com/exposit/web/sorokin/UserController.java
+++ src/main/java/com/exposit/web/sorokin/UserController.java
@@ -1,10 +1,27 @@
 package com.exposit.web.sorokin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.exposit.domain.model.sorokin.User;
+import com.exposit.domain.service.sorokin.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = { "", "/" })
+	public ModelAndView message() {
+		ModelAndView mav = new ModelAndView("users");
+		List<User> users = userService.getUsers();
+		mav.addObject("listOfUsers", users);
+		return mav;
+	}
 }
