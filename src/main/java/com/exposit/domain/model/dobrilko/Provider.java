8e3c9ed1eb3854b8a78ac28ea4c96ce4de9054dc
Kate Dobrilko <katedob1595@gmail.com>
2015-08-18 01:49:12 +0300

    Provider validation

--- src/main/java/com/exposit/domain/model/dobrilko/Provider.java
+++ src/main/java/com/exposit/domain/model/dobrilko/Provider.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.exposit.domain.model.zanevsky.Module;
 
@@ -20,12 +23,26 @@ public class Provider {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "provider_id")
 	private int id;
+	@Pattern(regexp = "[A-Z][a-zA-Z|| ||\\-||&||\"||0-9]*]*",
+			message = "Provider name should: 1. Start from capital letter"
+					+ "2. Contain only letters, numeric symbols, spaces,"
+					+ " quotes, dashes and ampersand.")
+	@Size(max = 50, message = "Provider name lenght must be less than 50.")
+	@NotNull(message = "Provider name is a mandatory.")
 	@Column(name = "provider_name", unique = true)
 	private String name;
+	@Pattern(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*"
+			+ "@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$",
+			message = "Email should match the pattern!")
 	@Column(name = "provider_email")
 	private String email;
+
+	@Pattern(regexp = "^[0-9]{12}$",
+			message = "Phone number must be 12 digits string!")
 	@Column(name = "phone")
 	private String phone;
+	@Pattern(regexp = "^[0-9]{6}$",
+			message = "Zip code must be 6 digits string!")
 	@Column(name = "zip_code")
 	private String zipCode;
 
